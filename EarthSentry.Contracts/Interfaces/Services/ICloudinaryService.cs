f20105962574be2624d24539a74d4b525f39b15b
salvalagio <salvalagio.ygor@gmail.com>
2025-06-14 20:33:34 -0300

    chore: remove unused service.

--- EarthSentry.Contracts/Interfaces/Services/ICloudinaryService.cs
+++
@@ -1,9 +0,0 @@
-﻿using EarthSentry.Contracts.Services;
-
-namespace EarthSentry.Contracts.Interfaces.Services
-{
-    public interface ICloudinaryService
-    {
-        Task<CloudinarySignInDto> SignIn();
-    }
-}
